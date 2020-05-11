447167cab9a9ac5cf909470cb2765a1e54162682
M Faried <m.a.faried@gmail.com>
2020-05-11 23:59:22 +0200

    Updates

---
+++ precache-manifest.f08cb7ed191c95d968e3677fda41f8a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa8a1c024f61d8a2d4fa0c998e90106",
+    "url": "/simple-todo-list/index.html"
+  },
+  {
+    "revision": "57518d4a9748c4c7d35e",
+    "url": "/simple-todo-list/static/css/main.869f5bae.chunk.css"
+  },
+  {
+    "revision": "56b7d8bbe869e2c18a22",
+    "url": "/simple-todo-list/static/js/2.303cffe4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-todo-list/static/js/2.303cffe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57518d4a9748c4c7d35e",
+    "url": "/simple-todo-list/static/js/main.217cae6b.chunk.js"
+  },
+  {
+    "revision": "b6add5cf896e0c2832b0",
+    "url": "/simple-todo-list/static/js/runtime-main.741340ab.js"
+  }
+]);
